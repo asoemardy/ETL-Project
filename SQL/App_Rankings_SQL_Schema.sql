dca588f379e9f0b3f7a80c8114274590b4704a67
Matthew Breda <breda.matt@gmail.com>
2020-12-01 17:03:21 -0800

    updates SQL code and PNG

--- SQL/App_Rankings_SQL_Schema.sql
+++ SQL/App_Rankings_SQL_Schema.sql
@@ -5,6 +5,7 @@
 CREATE TABLE "iOS_Apps" (
     "iOS_App_ID" INT   NOT NULL,
     "iOS_App_Name" VARCHAR(50)   NOT NULL,
+    "Publisher_ID" INT   NOT NULL,
     CONSTRAINT "pk_iOS_Apps" PRIMARY KEY (
         "iOS_App_ID"
      )
@@ -21,7 +22,8 @@ CREATE TABLE "Publishers" (
 CREATE TABLE "iOS_Daily_Rankings" (
     "iOS_Log_Num" SERIAL   NOT NULL,
     "iOS_Date" Date   NOT NULL,
-    "iOS_Rank" INT   NOT NULL,
+    "iOS_Grossing_Rank" INT   NOT NULL,
+    "iOS_Download_Rank" INT   NOT NULL,
     "iOS_App_ID" INT   NOT NULL,
     "Publisher_ID" INT   NOT NULL,
     CONSTRAINT "pk_iOS_Daily_Rankings" PRIMARY KEY (
@@ -32,6 +34,7 @@ CREATE TABLE "iOS_Daily_Rankings" (
 CREATE TABLE "Android_Apps" (
     "Android_App_ID" INT   NOT NULL,
     "Android_App_Name" VARCHAR(50)   NOT NULL,
+    "Publisher_ID" INT   NOT NULL,
     CONSTRAINT "pk_Android_Apps" PRIMARY KEY (
         "Android_App_ID"
      )
@@ -40,7 +43,8 @@ CREATE TABLE "Android_Apps" (
 CREATE TABLE "Android_Daily_Rankings" (
     "Android_Log_Num" SERIAL   NOT NULL,
     "Android_Date" Date   NOT NULL,
-    "Android_Rank" INT   NOT NULL,
+    "Android_Grossing_Rank" INT   NOT NULL,
+    "Android_Download_Rank" INT   NOT NULL,
     "Android_App_ID" INT   NOT NULL,
     "Publisher_ID" INT   NOT NULL,
     CONSTRAINT "pk_Android_Daily_Rankings" PRIMARY KEY (
@@ -48,11 +52,8 @@ CREATE TABLE "Android_Daily_Rankings" (
      )
 );
 
-CREATE TABLE "Publishers_Apps" (
-    "Publisher_ID" INT   NOT NULL,
-    "iOS_App_ID" INT   NOT NULL,
-    "Android_App_ID" INT   NOT NULL
-);
+ALTER TABLE "iOS_Apps" ADD CONSTRAINT "fk_iOS_Apps_Publisher_ID" FOREIGN KEY("Publisher_ID")
+REFERENCES "Publishers" ("Publisher_ID");
 
 ALTER TABLE "iOS_Daily_Rankings" ADD CONSTRAINT "fk_iOS_Daily_Rankings_iOS_App_ID" FOREIGN KEY("iOS_App_ID")
 REFERENCES "iOS_Apps" ("iOS_App_ID");
@@ -60,18 +61,12 @@ REFERENCES "iOS_Apps" ("iOS_App_ID");
 ALTER TABLE "iOS_Daily_Rankings" ADD CONSTRAINT "fk_iOS_Daily_Rankings_Publisher_ID" FOREIGN KEY("Publisher_ID")
 REFERENCES "Publishers" ("Publisher_ID");
 
+ALTER TABLE "Android_Apps" ADD CONSTRAINT "fk_Android_Apps_Publisher_ID" FOREIGN KEY("Publisher_ID")
+REFERENCES "Publishers" ("Publisher_ID");
+
 ALTER TABLE "Android_Daily_Rankings" ADD CONSTRAINT "fk_Android_Daily_Rankings_Android_App_ID" FOREIGN KEY("Android_App_ID")
 REFERENCES "Android_Apps" ("Android_App_ID");
 
 ALTER TABLE "Android_Daily_Rankings" ADD CONSTRAINT "fk_Android_Daily_Rankings_Publisher_ID" FOREIGN KEY("Publisher_ID")
 REFERENCES "Publishers" ("Publisher_ID");
-
-ALTER TABLE "Publishers_Apps" ADD CONSTRAINT "fk_Publishers_Apps_Publisher_ID" FOREIGN KEY("Publisher_ID")
-REFERENCES "Publishers" ("Publisher_ID");
-
-ALTER TABLE "Publishers_Apps" ADD CONSTRAINT "fk_Publishers_Apps_iOS_App_ID" FOREIGN KEY("iOS_App_ID")
-REFERENCES "iOS_Apps" ("iOS_App_ID");
-
-ALTER TABLE "Publishers_Apps" ADD CONSTRAINT "fk_Publishers_Apps_Android_App_ID" FOREIGN KEY("Android_App_ID")
-REFERENCES "Android_Apps" ("Android_App_ID");
 
